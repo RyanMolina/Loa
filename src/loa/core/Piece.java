e4d7816e06fea4be62e1a67371f7762d84043b67
RyanMolina <ryangabrielpmolina@gmail.com>
2016-08-01 10:40:36 +0800

    Minimax Iterative Deepening

--- src/loa/core/Piece.java
+++ src/loa/core/Piece.java
@@ -1,20 +1,10 @@
 package loa.core;
 
-/** A Piece denotes the contents of a square, or identifies one side
- *  (Black or White) of a game.
- *  @author Maaz Uddin
- */
+
 public enum Piece {
-    /** The names of the pieces.  EMP indicates an empty square. The
-     *  arguments give names to the piece colors. */
+
     BP, WP, EMP;
 
-    /** Returns the full name of this piece (black, white, or
-     *  empty). */
-
-
-
-    /** Returns Piece with my opposing color (null for EMP). */
     Piece opposite() {
         switch (this) {
         case BP:
